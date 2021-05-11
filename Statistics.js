1b31bf59542a57fccb73136ef35e5c45ba85cb9d
KarolinaFilipiuk <k.filipiuk18@gmail.com>
2021-05-11 14:32:32 +0200

    add Statistics class

---
+++ Statistics.js
@@ -0,0 +1,34 @@
+// D - gamesStats []
+// M - addGameToStats()
+// M - showGamesStats()
+
+
+class Statistics {
+    constructor() {
+        this.gameStats = [{
+            win: true,
+            bid: 2
+        }, {
+            win: false,
+            bid: -10
+        }];
+    }
+
+    addGameToStats(win, bid) {
+        let gameStat = {
+            win,
+            bid,
+        }
+
+        this.gameStats.push(gameStat);
+    }
+
+    showGameStats() {
+        const games = this.gameStats.length;
+        const wins = this.gameStats.filter(game => game.win).length;
+        const losses = games - wins;
+        return [games, wins, losses]
+    }
+}
+
+// const stats = new Statistics();
