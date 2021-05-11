97e20f950da632d5bd1d308a91d2cfc536062d40
KarolinaFilipiuk <k.filipiuk18@gmail.com>
2021-05-11 14:43:48 +0200

    add Draw class

---
+++ Draw.js
@@ -0,0 +1,21 @@
+class Draw {
+    constructor() {
+        this.options = ["red", "green", "blue"];
+        let _result = this.drawResult();
+
+        this.getDrawResult = () => _result;
+    }
+
+    drawResult() {
+        let colors = [];
+        const numberColors = this.options.length
+        // uzupełnianie poprzez losowanie kolorów
+        for (let i = 0; i < numberColors; i++) {
+            const color = this.options[Math.floor(Math.random() * numberColors)];
+            colors.push(color)
+        }
+        return colors;
+    }
+}
+
+const draw = new Draw();
